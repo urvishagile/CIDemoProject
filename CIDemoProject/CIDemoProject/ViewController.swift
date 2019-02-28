8d52a7201e846e3bb64b5a7f8a3bd9b4ea78be0c
Urvish Patel <urvish.patel@agileinfoways.com>
2019-02-28 15:25:51 +0530

    upload command

--- CIDemoProject/CIDemoProject/ViewController.swift
+++ CIDemoProject/CIDemoProject/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblTest:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        lblTest.text = "Agile infoways pvt ltd"
     }
 
 
