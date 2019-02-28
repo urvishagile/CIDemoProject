a4542caf05c91f92adc27792e5d61305d4a044bc
Urvish Patel <urvish.patel@agileinfoways.com>
2019-02-28 16:32:53 +0530

    update

--- CIDemoProject/CIDemoProject/ViewController.swift
+++ CIDemoProject/CIDemoProject/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        lblTest.text = "jainam jaisal Shah"
+        lblTest.text = "jaisal Shah"
     }
 
 
