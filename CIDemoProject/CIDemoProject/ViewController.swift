72487a2bc64662120c26a3c97ebf1c2c29b4db36
Urvish Patel <urvish.patel@agileinfoways.com>
2019-02-28 12:53:02 +0530

    first commit

---
+++ CIDemoProject/CIDemoProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIDemoProject
+//
+//  Created by agileimac-5 on 28/02/19.
+//  Copyright © 2019 Urvish Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
